338dc43b264b0936fa98114d24198730f4c354b0
Panditax727 <juanpotito754@gmail.com>
2026-06-05 17:29:41 -0400

    chore: actualizar App.jsx y scrollbar personalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./features/auth/components/LoginPage";
 import RegisterPage from "./features/auth/components/RegisterPage";
 import ChatLayout from "./features/chat/components/ChatLayout";
+import ChannelsPage from "./features/channels/components/Channels";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/chat" element={<ChatLayout />} />
+        <Route path="/channels" element={<ChannelsPage />} />
       </Routes>
     </BrowserRouter>
   );
